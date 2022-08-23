0f1414e504c8f74505e50844a235b26246be7c2e
RoiKadari <roikadari@gmail.com>
2022-08-23 10:02:23 +0300

    13

--- src/pages/keywordsPages.js
+++ src/pages/keywordsPages.js
@@ -1,7 +1,7 @@
 import { keyWords331 } from "./lectures/331";
 import { keyWords13 } from "./lectures/13";
 
-const contacted = keyWords13.concat(keyWords331[0]);
+const contacted = keyWords13.concat(keyWords331);
 console.log(contacted);
 export const catTitles = 
 contacted.forEach(element =>
